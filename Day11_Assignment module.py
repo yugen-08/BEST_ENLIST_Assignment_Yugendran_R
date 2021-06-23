3679cf4ee9589b8bc40f7210036c3418f83d4745
yugen-08 <80707056+yugen-08@users.noreply.github.com>
2021-06-23 21:11:39 +0530

    Add files via upload

---
+++ Day11_Assignment module.py
@@ -0,0 +1,4 @@
+def mymodule(var):
+    print("Hi "+var)
+
+l=[1,2,3,4,5]
---
+++ Day11_Assignment part 2.py
@@ -0,0 +1,15 @@
+import scratch_13
+
+scratch_13.mymodule('mag')
+scratch_13.l[1]=8
+print("After updating : ",scratch_13.l)
+
+import random
+
+print(random.randint(1,100))
+print(random.random())
+
+import sys
+
+sys.path.append( 'C:/User/Admin/Music')
+print(sys.path)
